9ed9664f73d0e028cfd44c5120a69dba829a9992
Shiro <shahirul.ar@gmail.com>
2022-07-06 01:52:33 +0800

    Add pagination when retrieving data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Validator;
+use DB;
 
 class UserController extends Controller
 {
@@ -42,7 +43,7 @@ class UserController extends Controller
         if($id){
             $user = User::find($id);
         }else{
-            $user = User::all();
+            $user = DB::table('users')->select('name','email')->paginate(5);
         }
 
         return $user;    
